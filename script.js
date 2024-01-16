31692b801dde9041919a3bdf85952a8162714f10
Apurbo Sarkar <50476519+Apurb0@users.noreply.github.com>
2024-01-16 19:54:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let populationBangladesh = 173906485;
+console.log(populationBangladesh / 2);
+populationBangladesh++;
+
+console.log(populationBangladesh);
+
+const populationFinland = 5547884;
+const maxPopulation = populationBangladesh > populationFinland;
+
+console.log(maxPopulation);
+
+const avgPopulation = 33000000;
+const maxAvgPopulation = populationBangladesh < avgPopulation;
+
+console.log(maxAvgPopulation);
+
+const country = 'portugal';
+const continent = 'Europe';
+const peoplePortugal = 11;
+
+
+const description = `${country}is in ${continent}, and it's ${peoplePortugal} million people speak portuguese.`;
+
+console.log(description);
+
